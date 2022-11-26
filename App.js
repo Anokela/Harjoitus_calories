2f5d85e16a80121a6d4e2cfa0e2bd01a53ce7a89
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 18:51:26 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,13 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import React, {useState, useEffect} from 'react';
+
+const URL = 'http://www.boredapi.com/api/activity/';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [refresh, setRefresh] = useState();
+  const [activity, setActivity] = useState('');
+  const [type, setType] = useState('');
+  const [participants, setParticipants] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    fetch(URL)
+     .then(response => response.json())
+     .then ((json) => {
+        const data = json;
+        setActivity(firstLetterToLowerCase(data.activity));
+        setType(data.type);
+        setParticipants(data.participants);
+        setPrice(data.price)
+        setError(null);
+        setIsLoading(false);
+     }, (error) => {
+          setError("Error retrieving the activity!");
+          setIsLoading(false);
+          console.log(error);
+     })
+   }, [refresh])
+
+  const getNewActivity = () => {
+    setRefresh({});
+  }
+
+  const chekPrefix = (str) => {
+    if (str.charAt(0) === 'a' || str.charAt(0) === 'e' || str.charAt(0) === 'i' || str.charAt(0) === 'o' || str.charAt(0) === 'u') {
+      return 'an';
+    } else {
+      return 'a ';
+    }
+  }
+
+  const firstLetterToLowerCase = (str) => {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
+  if (isLoading) {
+    return <View style={styles.container}><ActivityIndicator size='large'/></View>
+  }
+  else if (error) {
+    return <View style={styles.container}><Text>{error}</Text></View>
+  }
+  else {
+    return (
+      <View style={styles.container}>
+              <Text style={styles.heading}>I'm so bored...</Text>
+        <Text style={styles.text}>As {chekPrefix(type)} {type} type of activity {activity}. 
+          It takes {participants === 1 ? participants + ' participant' : participants + ' participants' } 
+          and {price === 0 ? 'does not cost anything' : 'is propably quite cheap'}.
+         </Text>
+      <Button style={styles.button} onPress={() => getNewActivity()} title='Refresh'></Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +73,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  text: {
+    marginLeft: 10,
+    marginRight:10,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 30,
+    marginBottom: 30,
   },
 });
